9c2dc75d98fb55ecc492f2407e20cdd09b6aec56
saur0w <sthapliyal085@gmail.com>
2026-07-20 11:37:08 +0530

    "Basic Structure"

---
+++ src/components/Stage/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./style.module.scss";
+
+export default function Studio() {
+    return (
+        <section className={styles.studio}>
+
+        </section>
+    );
+}

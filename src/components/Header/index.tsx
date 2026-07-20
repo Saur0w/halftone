4059623a531c331b4c971bd879cee2f4bd25da63
saur0w <sthapliyal085@gmail.com>
2026-07-20 14:23:30 +0530

    "Header Structure"

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import styles from "./style.module.scss";
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            
+        </header>
+    );
+}
